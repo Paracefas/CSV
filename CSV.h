806ac2ab99edfab0d71f12a27d3bfc3d28230daf
= <=>
2019-08-05 15:39:07 -0300

    Add into paracefas namespace

--- CSV.h
+++ CSV.h
@@ -1,35 +1,26 @@
 #pragma once
 #include <fstream>
 #include <vector>
-#include <variant>
 
-// using Data = std::variant<
-// 					int, 
-// 					double, 
-// 					long long, 
-// 					unsigned long long, 
-// 					long, 
-// 					unsigned long, 
-// 					std::string
-// 			>;
+namespace paracefas {
+	class CSV {
+		std::ifstream raw_data;
+		size_t row_size = 0;
+		size_t last_line = 0;
+		std::vector<std::string> data;
+		std::string file_name;
 
-class CSV {
-	std::ifstream raw_data;
-	size_t row_size = 0;
-	size_t last_line = 0;
-	std::vector<std::string> data;
-	std::string file_name;
+		void process();
+		int findByValue(std::string);
+		void processTitles(std::string);
+		void processValues(std::string);
+		bool checkInput(std::string);
 
-	void process();
-	int findByValue(std::string);
-	void processTitles(std::string);
-	void processValues(std::string);
-	bool checkInput(std::string);
-
-public:
-	CSV(std::string);
-	CSV(char const*);
-	~CSV();
-	std::string Get(std::string, size_t); /* Name of column, number of row */
-	void Post(std::string); /* Data src, number of row */
-};
+	public:
+		CSV(std::string);
+		CSV(char const*);
+		~CSV();
+		std::string Get(std::string, size_t); /* Name of column, number of row */
+		void Post(std::string); /* Data src, number of row */
+	};
+} // end namespace 
