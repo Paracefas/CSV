62c3914d3755890399de0c85493e25f490664ffb
= <=>
2019-08-05 07:06:25 -0300

    First commit

---
+++ CSV.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <fstream>
+#include <vector>
+#include <variant>
+
+// using Data = std::variant<
+// 					int, 
+// 					double, 
+// 					long long, 
+// 					unsigned long long, 
+// 					long, 
+// 					unsigned long, 
+// 					std::string
+// 			>;
+
+class CSV {
+	std::ifstream raw_data;
+	size_t row_size = 0;
+	size_t last_line = 0;
+	std::vector<std::string> data;
+	std::string file_name;
+
+	void process();
+	int findByValue(std::string);
+	void processTitles(std::string);
+	void processValues(std::string);
+	bool checkInput(std::string);
+
+public:
+	CSV(std::string);
+	CSV(char const*);
+	~CSV();
+	std::string Get(std::string, size_t); /* Name of column, number of row */
+	void Post(std::string); /* Data src, number of row */
+};
